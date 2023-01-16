3b7ff6b211535a786e457cf2c930e2691bac9300
GamerNoTitle <GamerNoTitle@outlook.com>
2023-01-16 22:50:30 +0800

    init

---
+++ static/config.js
@@ -0,0 +1,5 @@
+config = {
+    "alipay": "https://cdn.bilicdn.tk/gh/GamerNoTitle/Picture-repo-v1@master/img/Payments/AliPay.jpg",
+    "wechat": "https://cdn.bilicdn.tk/gh/GamerNoTitle/Picture-repo-v1@master/img/Payments/WeChat.png",
+    "afdian": "https://afdian.net/a/GamerNoTitle"
+}
