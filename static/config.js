6356c6c72ed486d9a53bcd2fb1d148b460317e9b
GamerNoTitle <GamerNoTitle@outlook.com>
2023-07-15 23:29:33 +0800

    Updated Sponsors

--- static/config.js
+++
@@ -1,7 +0,0 @@
-config = {
-    "alipay": "https://cdn.bilicdn.tk/gh/GamerNoTitle/Picture-repo-v1@master/img/Payments/AliPay.jpg",
-    "wechat": "https://cdn.bilicdn.tk/gh/GamerNoTitle/Picture-repo-v1@master/img/Payments/WeChat.png",
-    "afdian": "https://afdian.net/a/GamerNoTitle",
-    "paypal": "https://paypal.me/GamerNoTitle",
-    "steam": "https://steamcommunity.com/tradeoffer/new/?partner=1069144147&token=yR270bu0"
-}
